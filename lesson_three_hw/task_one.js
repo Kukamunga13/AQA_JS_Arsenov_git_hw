6efbb834404401d130571f311ad0dd603e964b25
unknown <kukamunga2002@yandex.ru>
2024-05-23 01:31:45 +0300

    Task one solution

---
+++ lesson_three_hw/task_one.js
@@ -0,0 +1,36 @@
+let item_1; //1
+let item_2; //4
+let item_3; //7
+let item_4; //10
+let item_5; //15
+let item_6; //17
+let item_7; //22
+let item_6_type; //18
+let item_7_type //23
+
+item_1 = 5; //2
+console.log(item_1); // 3
+
+item_2 = 3; //5
+console.log(item_2); //6 
+
+item_3 = item_1 + item_2; //8
+console.log(item_3); //9
+
+item_4 = 'Yolochka'; //11
+console.log(item_4); //12
+
+console.log(item_3 + item_4); //13
+console.log(item_3 * item_4); //14
+
+item_5 = item_3; //16
+
+item_6 = 15; //19
+
+item_6_type = typeof item_6; //20
+console.log('item_6 == ' + item_6, + 'item_6_type == ' + item_6_type); //21
+
+item_7 = String(item_6); //22
+
+item_7_type = typeof item_7; //24
+console.log('item_7 == ' + item_7, + 'item_7_type == ' + item_7_type);
