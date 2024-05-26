354ef0111e36f0e87b6e9553f756fb85b0dda066
unknown <kukamunga2002@yandex.ru>
2024-05-26 19:44:10 +0300

    Corrected the comment assignment. Added a check to see if the value is a number

--- lesson_three_hw/task_three.js
+++ lesson_three_hw/task_three.js
@@ -1,12 +1,19 @@
 let numberEnteredByTheUser = prompt('введите число');
-numberEnteredByTheUser;
 
-if (numberEnteredByTheUser > 0) {
-  if (numberEnteredByTheUser % 2 >0) {
-    alert('Введите чётное число');
+if (numberEnteredByTheUser / 1 > 0) {
+  if (numberEnteredByTheUser > 0) {
+    if (numberEnteredByTheUser % 2 > 0) {
+      alert ('Введите чётное число');
+    } else {
+      alert(numberEnteredByTheUser  + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + String(numberEnteredByTheUser));  
+    }
   } else {
-    alert(numberEnteredByTheUser  + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + String(numberEnteredByTheUser));
+    alert ('Число должно быть больше 0!');
   }
 } else {
-  alert('Число должно быть больше 0!');
+  alert ('Введите число');
 }
+
+
+
+
