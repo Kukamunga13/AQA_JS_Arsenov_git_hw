99ed4bf8e0ec495a55b92e4ec009245f2247df9a
unknown <kukamunga2002@yandex.ru>
2024-05-26 22:46:16 +0300

    Corrected the comment assignment.Removed unnecessary number check

--- lesson_three_hw/task_three.js
+++ lesson_three_hw/task_three.js
@@ -2,7 +2,7 @@
   let numberEnteredByTheUser = prompt('введите число');
   const dataType = Number(numberEnteredByTheUser);
   
-  if (dataType / 1 > 0) {
+  if (dataType > 0) {
     if (Number.isInteger(dataType)) {
         alert(numberEnteredByTheUser  + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + ' ' + String(numberEnteredByTheUser) + String(numberEnteredByTheUser) + String(numberEnteredByTheUser)); 
     } else {
