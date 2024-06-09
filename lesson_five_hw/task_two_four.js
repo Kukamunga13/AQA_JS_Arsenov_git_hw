22990011e73fd258313884ff7a264ba91b8e20d4
unknown <a.arsyonov@qsoft.ru>
2024-06-09 17:11:50 +0300

    Completed task 2.4

---
+++ lesson_five_hw/task_two_four.js
@@ -0,0 +1,9 @@
+
+let originalProposal = 'Привет Мир Тест Первый ntcn тест Тест';
+let brokenDownIntoWordsOriginalProposal = originalProposal.toLowerCase().split(" ");
+let capitalizedOriginalProposal = brokenDownIntoWordsOriginalProposal.map(привет=> {
+    return привет[0].toUpperCase() + привет.substring(1);
+})
+let stringCapitalizedOriginalProposal = capitalizedOriginalProposal.join(" "); 
+let finelOriginalProposal = stringCapitalizedOriginalProposal.replaceAll(' ', '');
+console.log(finelOriginalProposal[0].toLowerCase() + finelOriginalProposal.slice(1));
